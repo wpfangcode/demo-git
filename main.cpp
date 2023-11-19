5851ad422f96a6c4b38125dfe5d75a5006895e1e
wpfangcode <79435939+wpfangcode@users.noreply.github.com>
2023-11-19 13:37:03 +0800

    Create main.cpp
    the main code

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//#define __DEBUG
+
+int main()
+{
+ 
+  int x=5;
+       
+  #ifdef __DEBUG
+      cerr<<"x: "<<x<<endl;
+      asm("int $3");  
+  #endif
+  #ifdef __ADD
+       asm("add $1,%1" : "=r"(x) :"r"(x));
+  #endif
+   x+=6;
+   cout<<x<<endl;S
+    return 1;
+}
